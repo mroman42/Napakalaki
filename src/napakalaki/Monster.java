a4f3425b0a2afa679c19e0fbcd9ef7f42d390d50
JCEntrena <jose@jose>
2014-02-27 22:16:57 +0100

    Completadas las Clases con los Métodos consultores e impresores.

--- src/napakalaki/Monster.java
+++ src/napakalaki/Monster.java
@@ -1,12 +1,39 @@
 package napakalaki;
 
 /**
- *
+ * @date 27/02/14
  * @author Mario Román
+ * @author José Carlos Entrena
  */
 public class Monster {
     private String name;
     private int level;
+    private BadConsequence bad; 
+    private Prize price; 
+    
+    // Constructores
+    public Monster(String name, int level, BadConsequence bad, Prize price){
+        this.name = name; 
+        this.level = level; 
+        this.bad = bad; 
+        this.price = price; 
+    }
     
     // Consultores
+    public String getName() {
+        return name; 
+    }
+    
+    public int getLevel() {
+        return level; 
+    }
+    
+    // Métodos
+    @Override
+    public String toString() {
+        return "Name = " + name + "\nLevel = " + Integer.toString(level) + 
+                "\nBad: " + bad.toString() + "\nPrize: " + price.toString()
+                + ".\n";
+    }
+    
 }
