4c62d08a0368212b680c1f5235a2affb908d6749
JCEntrena <jentrena@correo.ugr.es>
2014-03-19 20:53:13 +0100

    Creadas nuevas clases.

---
+++ src/napakalaki/CombatResult.java
@@ -0,0 +1,11 @@
+package napakalaki;
+
+/**
+ * @date 19/3/14 
+ * @author Mario Román 
+ * @author José Carlos Entrena
+ * @author Óscar Bermúdez
+ */
+public enum CombatResult {
+    WINANDWINGAME, WIN, LOSE, LOSEANDESCAPE, LOSEANDDIE    
+}
---
+++ src/napakalaki/Game.java
@@ -0,0 +1,11 @@
+package napakalaki;
+
+/**
+ * @date 19/3/14
+ * @author José Carlos Entrena
+ * @author Mario Román
+ * @author Óscar Bermúdez
+ */
+public class Game {
+    
+}
