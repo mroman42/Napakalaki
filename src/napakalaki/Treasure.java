b16d34afd91273aee2eb1c6b46c98ecce4dd8429
oxcar103 <oxcar103@outlook.com>
2014-03-20 10:58:43 +0100

    Clase Treasure implementada
    No sé para qué sirve declarar los datos miembro como "final" y tampoco
    estoy seguro de saber lo que debe hacer getType()

--- src/napakalaki/Treasure.java
+++ src/napakalaki/Treasure.java
@@ -7,5 +7,35 @@ package napakalaki;
  * @author Óscar Bermúdez
  */
 public class Treasure {
+    private final String name;
+    private final int goldCoins;
+    private final int minBonus;
+    private final int maxBonus;
+
+    public Treasure(String name, int goldCoins, int minBonus, int maxBonus) {
+        this.name = name;
+        this.goldCoins = goldCoins;
+        this.minBonus = minBonus;
+        this.maxBonus = maxBonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGoldCoins() {
+        return goldCoins;
+    }
+
+    public int getMinBonus() {
+        return minBonus;
+    }
+
+    public int getMaxBonus() {
+        return maxBonus;
+    }
     
+    public TreasureKind getType(){
+        return  TreasureKind.valueOf(name);
+    };
 }
