71ba81644708c9cd8e9da301d1b645d8a109c3b7
oxcar103 <oxcar103@outlook.com>
2014-03-20 11:09:19 +0100

    Clase Dice
    Estructura hecha, falta implemetar los métodos

--- src/napakalaki/Dice.java
+++ src/napakalaki/Dice.java
@@ -7,5 +7,15 @@ package napakalaki;
  * @author Óscar Bermúdez
  */
 public class Dice {
+    private static final Dice instance = new Dice();
     
+    // El constructor privado asegura que no se puede instanciar
+    // desde otras clases
+    private Dice() {}
+    
+    public static Dice getInstance() {
+        return instance;
+    }
+    
+    public int nextNumber(){return 0;}
 }
