a1f1fc43ea1b0d9ac1188dea88440004ad030310
M42 <mromang08@gmail.com>
2014-04-17 13:28:47 +0200

    Lectura según el guión. Funciones de lectura de cadenas y números según el guión.

--- src/TextUI/TextUI.java
+++ src/TextUI/TextUI.java
@@ -1,6 +1,9 @@
 package TextUI;
 
 import Game.Napakalaki;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -26,6 +29,44 @@ public class TextUI {
         ArrayList<String> names = readNames();
         napakalaki.initGame(names);
     }
+    
+    
+    /**
+     * Lee un número introducido por el usuario.
+     * @param msg Mensaje mostrado por pantalla para solicitar la entrada. 
+     */
+    public  int readInteger(String msg) {
+        int input = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       
+        System.out.print(msg);
+        
+        try {
+            input = Integer.parseInt(br.readLine());
+        } catch (NumberFormatException nfe){
+            System.err.println("Invalid Format!");
+        }
+        catch (IOException ioe){
+            System.err.println("IO Error!");            
+        }
+        return input;
+    }
+    
+    /**
+     * Lee una cadena introducida por el usuario.
+     * @param msg Mensaje mostrado por pantalla para solicitar la entrada. 
+     */
+    public  String readString(String msg) {
+        String input= "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       
+        System.out.print(msg);
+        try{
+            input = br.readLine();
+        }
+        catch(IOException ioe){
+            System.err.println("IO Error!");            
+        }
+        return input;
+    }   
     
     private static ArrayList<String> readNames() {
         System.out.println("Introduzca los nombres de los jugadores.");
