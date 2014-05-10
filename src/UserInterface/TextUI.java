8083a3306c4185a4671b37f0f1e9a49f6fa120d9
M42 <mromang08@gmail.com>
2014-05-10 23:29:50 +0200

    Unused imports.

--- src/UserInterface/TextUI.java
+++ src/UserInterface/TextUI.java
@@ -13,8 +13,6 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @date 10/04/14
