3ca1546d7ee2f0c455eda27fdd4a96a56ae94d4d
JCEntrena <jentrena@correo.ugr.es>
2014-05-15 12:27:00 +0200

    Creando métodos.

--- src/Game/CultistPlayer.java
+++ src/Game/CultistPlayer.java
@@ -6,19 +6,44 @@ import java.util.ArrayList;
 
 /**
  *
- * @author jose
+ * @author Mario Román
+ * @author José Carlos Entrena
+ * @author Óscar Bermúdez
  */
 public class CultistPlayer extends Player {
-    private boolean dead;
-    private final String name;
-    private int level;
-    private BadConsequence pendingBadConsequence;
-    private final ArrayList<Treasure> hiddenTreasures;
-    private final ArrayList<Treasure> visibleTreasures;
+    
+    private static int totalCultistPlayers = 0; 
+    private Cultist myCultistCard; 
+    
+    public CultistPlayer(Player p, Cultist c){
+        
+    }
     
     public void applyPrize(Prize prize){
         super.applyPrize(prize);
     }
+    
+    public int computeGoldCoinsValue(ArrayList<Treasure> treasure_list){
+        int gold_coins = 0; 
+        for (Treasure t : treasure_list)
+            gold_coins += (t.getGoldCoins()*2); 
+        return (gold_coins / 1000); 
+    }
+    
+    public boolean shouldConvert(){
+        return false; 
+    }
+    
+    public int getOponentLevel(Monster m){
+        return 0; 
+    }
+    
+    public int getCombatLevel(){
+        return super.getCombatLevel() + myCultistPlayer.getSpecialValue(); 
+    }
  
+    public static int getTotalCultistPlayers(){
+        return totalCultistPlayers; 
+    }
     
 }
