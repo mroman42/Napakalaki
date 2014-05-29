df0426d232cdaa0c71a1428a11e6ed01b6ec2288
M42 <mromang08@gmail.com>
2014-05-29 13:15:56 +0200

    TreasureView con labels.

--- src/GUI/TreasureView.java
+++ src/GUI/TreasureView.java
@@ -40,19 +40,69 @@ public class TreasureView extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        treasureNameLabel = new javax.swing.JLabel();
+        treasureKindLabel = new javax.swing.JLabel();
+        minBonusLabel = new javax.swing.JLabel();
+        maxBonusLabel = new javax.swing.JLabel();
+        priceLabel = new javax.swing.JLabel();
+
+        treasureNameLabel.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
+        treasureNameLabel.setText("Treasure Name");
+
+        treasureKindLabel.setFont(new java.awt.Font("Ubuntu", 2, 15)); // NOI18N
+        treasureKindLabel.setText("Treasure Kind");
+
+        minBonusLabel.setText("Min Bonus:  0");
+
+        maxBonusLabel.setText("Max Bonus: 0");
+
+        priceLabel.setText("Price: 0 $");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(treasureKindLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(12, 12, 12)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(maxBonusLabel)
+                                    .addComponent(minBonusLabel)))
+                            .addComponent(priceLabel))
+                        .addGap(0, 0, Short.MAX_VALUE))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(treasureNameLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(46, 46, 46))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(treasureNameLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(treasureKindLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(minBonusLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(maxBonusLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(priceLabel)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel maxBonusLabel;
+    private javax.swing.JLabel minBonusLabel;
+    private javax.swing.JLabel priceLabel;
+    private javax.swing.JLabel treasureKindLabel;
+    private javax.swing.JLabel treasureNameLabel;
     // End of variables declaration//GEN-END:variables
 }
