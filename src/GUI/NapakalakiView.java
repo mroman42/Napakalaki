0aa0a172d49f4118671cd442296fc2559766d496
JCEntrena <jentrena@correo.ugr.es>
2014-06-06 22:36:31 +0200

    Implementando.

--- src/GUI/NapakalakiView.java
+++ src/GUI/NapakalakiView.java
@@ -51,7 +51,12 @@ public class NapakalakiView extends javax.swing.JFrame {
             }
         });
 
-        buttonNextTurn.setText("SIguente Turno");
+        buttonNextTurn.setText("Siguente Turno");
+        buttonNextTurn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonNextTurnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -95,7 +100,14 @@ public class NapakalakiView extends javax.swing.JFrame {
         CombatResult result = napakalakiModel.combat(); 
         if (result == CombatResult.WINANDWINGAME)
             labelCombatResult.setText("¡Enhorabuena, has ganado!");
+        repaint(); 
     }//GEN-LAST:event_buttonCombatActionPerformed
+
+    private void buttonNextTurnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonNextTurnActionPerformed
+        if (napakalakiModel.nextTurnAllowed())
+            napakalakiModel.nextTurn();
+        repaint(); 
+    }//GEN-LAST:event_buttonNextTurnActionPerformed
 
 
     
